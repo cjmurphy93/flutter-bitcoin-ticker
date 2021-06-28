1ed6f5c38615434ede53414302cf359ef7d8f1f7
cjmurphy93 <cjmurph93@gmail.com>
2021-06-28 12:16:33 -0400

    added dropdown

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -42,7 +42,25 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+              items: [
+                DropdownMenuItem(
+                  child: Text('USD'),
+                  value: 'USD',
+                ),
+                DropdownMenuItem(
+                  child: Text('EUR'),
+                  value: 'EUR',
+                ),
+                DropdownMenuItem(
+                  child: Text('GBP'),
+                  value: 'GBP',
+                ),
+              ],
+              onChanged: (value) {
+                print(value);
+              },
+            ),
           ),
         ],
       ),
